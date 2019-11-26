56448404d5c45f66283f62f7910a0a0796bcbf86
Seamile <lanhuermao@gmail.com>
2019-11-26 17:58:51 +0800

    添加了 api 接口

--- user/apis.py
+++ user/apis.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def get_vcode(request):
+    '''获取短信验证码'''
+    return
+
+
+def submit_vcode(request):
+    '通过验证码登录、注册'
+    return
+
+
+def get_profile(request):
+    '获取个人资料'
+    return
+
+
+def set_profile(request):
+    '修改个人资料'
+    return
+
+
+def upload_avatar(request):
+    '头像上传'
+    return
