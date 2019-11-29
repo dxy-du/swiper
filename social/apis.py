788aff70f01c1b7bf5694fc68d31d3e1c683f621
Seamile <lanhuermao@gmail.com>
2019-11-29 16:21:13 +0800

    创建了 Swiped 和 Friend 模型

--- social/apis.py
+++ social/apis.py
@@ -7,3 +7,30 @@ def rcmd_users(request):
     users = logics.rcmd(request.uid)
     rcmd_data = [user.to_dict() for user in users]
     return render_json(rcmd_data)
+
+
+def like(request):
+    '''右滑：喜欢'''
+    return render_json()
+
+
+def superlike(request):
+    '''上滑：超级喜欢'''
+    return render_json()
+
+
+def dislike(request):
+    '''左滑：不喜欢'''
+    return render_json()
+
+
+def rewind(request):
+    return render_json()
+
+
+def who_liked_me(request):
+    return render_json()
+
+
+def friend_list(request):
+    return render_json()
--- social/models.py
+++ social/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Swiped(models.Model):
+    '''滑动记录'''
+    STYPE = (
+        ('like', '右滑：喜欢'),
+        ('superlike', '上滑：超级喜欢'),
+        ('dislike', '左滑：不喜欢'),
+    )
+    uid = models.IntegerField(verbose_name='滑动者的 ID')
+    sid = models.IntegerField(verbose_name='被滑动者的 ID')
+    stype = models.CharField(max_length=10, choices=STYPE, verbose_name='滑动类型')
+    stime = models.DateTimeField(auto_now_add=True, verbose_name='滑动时间')
+
+
+class Friend(models.Model):
+    '''好友关系表'''
+    uid1 = models.IntegerField(verbose_name='好友ID')
+    uid2 = models.IntegerField(verbose_name='好友ID')
+
+    @classmethod
+    def make_friends(cls, uid1, uid2):
+        '''创建好友关系'''
+        uid1, uid2 = (uid2, uid1) if uid1 > uid2 else (uid1, uid2)
+        cls.objects.create(uid1=uid1, uid2=uid2)
