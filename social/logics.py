af9a6d7fe2d813eb1e526b97764d89d3b6ea6257
Seamile <lanhuermao@gmail.com>
2019-12-02 09:57:36 +0800

    为用户推荐接口添加了排除滑过的人处理

--- social/logics.py
+++ social/logics.py
@@ -7,20 +7,23 @@ from social.models import Friend
 
 
 def rcmd(uid):
+    '''用户推荐'''
     profile, _ = Profile.objects.get_or_create(id=uid)  # 获取用户的交友资料
 
     today = datetime.date.today()
     earliest_birthday = today - datetime.timedelta(profile.max_dating_age * 365)  # 最早出生日期
     latest_birthday = today - datetime.timedelta(profile.min_dating_age * 365)  # 最晚出生日期
 
+    # 取出已经滑过的人的 ID
+    sid_list = Swiped.objects.filter(uid=uid).values_list('sid', flat=True)
+
+    # 取出需要的用户，同时排除已经滑过的用户
     users = User.objects.filter(
         gender=profile.dating_gender,
         location=profile.dating_location,
         birthday__gte=earliest_birthday,
         birthday__lte=latest_birthday,
-    )[:20]
-
-    # TODO: 排除已经滑过的用户
+    ).exclude(id__in=sid_list)[:20]
 
     return users
 
